f931a6c542fbaa66fff6dc0c96c36633efd96b17
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-06 16:57:21 -0500

    Prompt written.

--- CPP-PRG-11-01-Movie-Data/main.cpp
+++ CPP-PRG-11-01-Movie-Data/main.cpp
@@ -5,11 +5,26 @@
 //  Created by Keith Smith on 11/6/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a program that uses a structure named MovieData to store the following
+//  information about a movie:
+//
+//      Title
+//      Director
+//      Year Released
+//      Running Time (in minutes)
+//
+//  The program should create two MovieData variables, store values in their members,
+//  and pass each one, in turn to a function that displays the infomation about the movie
+//  in a clearly formatted manner.
+//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+
+    
     return 0;
 }
